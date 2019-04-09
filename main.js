a25966d8d50a85a16dff4dee3e5ecb16da48dc4f
Raphael Amadeu <30945011+RaphaelAmadeu@users.noreply.github.com>
2019-04-09 17:58:38 -0300

    Initial version done

--- main.js
+++ main.js
@@ -266,7 +266,7 @@ var BANDS = [
         heading: "Experience",
         content: "After working on several personal projects, I've been hired to work at IBM in 2017, as a Front-end Developer. \n\nMy work can be seen across many projects, some of them are: \nCloud, Security, Events and Analytics.\n\nOne highlight certainly would be the Web coverage of IBM Cloud Discovery 2018, the biggest IBM event on Brazil to this day.\n\nSkills and Technologies used:",
         img: "./assets/experience.jpeg",
-        alt: "A picture of me",
+        alt: "Writing code into a whiteboard",
         alternate: true,
         list: true,
         listContent: [
@@ -293,7 +293,7 @@ var BANDS = [
         heading: "Skills",
         content: "Some of the skills I've obtained are:",
         img: "./assets/education.png",
-        alt: "A picture of me",
+        alt: "Hello world in JavaScript",
         alternate: false,
         list: true,
         listContent: [
